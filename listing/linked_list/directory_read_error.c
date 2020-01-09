655fceca0420ae3acb4e5e47844176ab269fb01a
gbogard <guillaume.bogard-coquard@epitech.eu>
2020-01-09 04:32:51 +0100

    removed permission denied error handling as it wasn't working

--- listing/linked_list/directory_read_error.c
+++ listing/linked_list/directory_read_error.c
@@ -11,16 +11,13 @@
 
 void directory_error(int error, const char *pathway)
 {
-    if (error == EPERM || error == ENOENT) {
+    if (error == ENOENT) {
         my_putstr(2, "./my_ls: cannot access ");
         my_putchar(2, '\'');
         my_putstr(2, pathway);
         my_putchar(2, '\'');
-    }
-    if (error == EPERM)
-        my_putstr(2, ": Permission denied\n");
-    if (error == ENOENT)
         my_putstr(2, ": No such file or directory\n");
+    }
     if (error == ENOTDIR) {
         my_putstr(1, pathway);
         my_putchar(1, '\n');
