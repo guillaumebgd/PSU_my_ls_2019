a1ae7e9f7162afe0b264fcb49cd3ad29e12d11f9
gbogard <guillaume.bogard-coquard@epitech.eu>
2020-01-16 01:47:14 +0100

    Update: usage of strerror to handle any error type

--- listing/linked_list/directory_read_error.c
+++ listing/linked_list/directory_read_error.c
@@ -2,24 +2,21 @@
 ** EPITECH PROJECT, 2019
 ** PSU_my_ls_2019
 ** File description:
-** treats errors when a file couldn't be read
+** treats errors when a file couldn't be
 */
 
 #include "my.h"
 #include <errno.h>
+#include <string.h>
 #include <unistd.h>
 
 void directory_error(const int error, const char *pathway)
 {
-    if (error == ENOENT) {
-        my_putstr(2, "./my_ls: cannot access ");
-        my_putchar(2, '\'');
-        my_putstr(2, pathway);
-        my_putchar(2, '\'');
-        my_putstr(2, ": No such file or directory\n");
-    }
-    if (error == ENOTDIR) {
-        my_putstr(1, pathway);
-        my_putchar(1, '\n');
-    }
+    my_putstr(2, "./my_ls: cannot access ");
+    my_putchar(2, '\'');
+    my_putstr(2, pathway);
+    my_putchar(2, '\'');
+    my_putstr(2, ": ");
+    my_putstr(2, strerror(errno));
+    my_putchar(2, '\n');
 }
